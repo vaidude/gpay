761fe475d73dc61aade387a1234ac748f104011d
sooraj <tpsooraj7@gmail.com>
2024-07-02 15:01:36 +0530

    authentication added properly

--- gpay/apk/views.py
+++ gpay/apk/views.py
@@ -3,9 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render, redirect,get_object_or_404
 from .models import *
+from django.contrib.auth.decorators import login_required
 
 def indx(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
     return render(request,'index.html')
+
+@login_required(login_url='index')
 def home(request):
     return render(request,'home.html')
 
